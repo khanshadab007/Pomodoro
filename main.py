a001da38d81b12efff19e1a1aa5e8b0bd41cf08b
khanshadab007 <khanshadabqasim@gmail.com>
2024-07-24 13:34:54 +0530

    Added UI

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -8,10 +9,24 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50,bg=YELLOW)
+
+canvas = Canvas(width=200, height=230, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 115, image=tomato_img,)
+canvas.pack()
+canvas.create_text(103, 130, text="00:00",fill= "white",font=(FONT_NAME, 28, "bold"))
+
+
+
+
+window.mainloop()
